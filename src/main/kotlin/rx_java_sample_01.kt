988cc7af8249c6bd388718a4a4fb71b71323a47d
NikolayTkachenko <tkachenni@mail.ru>
2024-06-26 00:52:06 +0300

    Update code

--- src/main/kotlin/rx_java_sample_01.kt
+++ src/main/kotlin/rx_java_sample_01.kt
@@ -8,4 +8,33 @@ fun rxJavaSampleTest01() {
 }
 
 //RX JAVA
+//Observable
+//Observer (Subscriber)
+//Operators
+//Subscription
+//Schedulers
+
+//Type observers
+//1-Observable
+//2-Single
+//3-Completable
+
+//4-Maybe
+//5-Flowable
+
+//6-Flow
+
+//Schedulers
+//Schedulers.io()
+//Schedulers.computation()
+//Schedulers.newThread()
+//Schedulers.single()
+//Schedulers.from(Executor executor)
+//AndroidSchedulers.mainThread()
+
+//Subjects - горячий
+//1.PublishSubject - не хранит событие
+//2.BehaviorSubject - хранит только последнее событие
+//3.ReplaySubject - хранит все события
+//4.AsyncSubject - хранит и испускает только последнее событие
 
